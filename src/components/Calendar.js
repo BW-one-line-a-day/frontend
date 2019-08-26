ad13b27ad164c76eaea6da1b208a1ef3c70195a5
Brad <Brad@BRADSPC>
2019-08-26 16:50:44 -0600

    Added today/yesterday components, calender is bugged
    Co-authored-by: Sheila Eichenberger <48662221+EpiceneDev@users.noreply.github.com>

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import DatePick from "./DatePick";
+import Axios from 'axios';
 
 export default function Calendar() {
+  const [date, setDate] = useState("")
+
+//   useEffect(() => {
+//     Axios
+//         .get(``)
+//         .then(res => {
+           
+//             setDate(res.data)
+//             console.log("res.data = ", res.data)
+//             console.log("res.date = ", res.data.date)})
+//         .catch(err => console.log("OOPS. API ERROR: ", err))
+// }, [date]);
+
 
   return(
-    <div>Calendar
+    <div className="react-calendar">
+      <div>Calendar</div>
+      {/* <DatePick date={date} setDate={setDate} /> */}
     </div>
   )
 }
--- src/components/Today.js
+++ src/components/Today.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import DailyEntry from "./DailyEntry"
 
 export default function Today() {
+  //axios request for list of previous entries on this day
+  const data = [{
+    "name": "Donald",
+    "content": "I'm a duck",
+    "date":["month 08", "day 10", "year 2019"]
+    },
+    {"name": "Daisy",
+    "content": "I'm another duck.",
+    "date":["month 08", "day 11", "year 2019"]
+    },
+    {"name": "Daisy",
+    "content": "",
+    "date":["month 08", "day 12", "year 2019"]
+    }]
 
+    let date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+
+    let todayDate = month + "-" + day + "-" + year;
+
+    const handleSubmit = (event) => {
+      //axios post call
+    }
   return(
-    <div>Today
+    //
+    <div>
+      <h2>Today</h2>
+      <h3>{todayDate}</h3>
+      <DailyEntry />
+      <button onSubmit={handleSubmit}>Submit!</button>
     </div>
   )
 }
