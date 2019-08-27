2c85a17bad07a4c34bcfc2d7392c1bbe3e931f89
Ian Belknap <45476409+Ian84Be@users.noreply.github.com>
2019-08-27 11:02:55 -0500

    fix main browserRouter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import 'semantic-ui-css/semantic.min.css';
 import App from "./App";
-// import { BrowserRouter as Router } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
@@ -14,9 +15,9 @@ import { reducer } from "./reducers";
 
 ReactDOM.render(
   // <Provider store={ store }>
-  //   <Router>
-  <App />,
-  //   </Router>
+    <Router>
+      <App />
+    </Router>,
   // </Provider>,
   document.getElementById("root")
 );
